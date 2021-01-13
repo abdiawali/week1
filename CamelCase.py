501c086258fd0c984bbf5f7358b629f19d03173b
abdiawali <abdiongithub2019>
2021-01-13 16:53:35 -0600

    lap completed

--- CamelCase.py
+++ CamelCase.py
@@ -1,19 +1,26 @@
+#this progrma asks users to enter string then changes first letter 
+#of each word to capital
+#then displays it
 
+#this function asks users to enter string then returns it
 def get_sentence():
     print('enter sentence: ')
     sentence=input()
     return sentence
 
+#splits the sentence and returns words as list
 def split_sentence(sentence):
     words=sentence.split()
     return words
 
+#changes each word's first letter to capital and returns it as list
 def modify(words):
     camelCase=[]
     for word in words:
         camelCase.append(word.capitalize())
     return camelCase
 
+# adds word list as string and displays it
 def display_sentence(camelcase):
     #for word in camelcase:
         #print(word)
@@ -24,7 +31,7 @@ def display_sentence(camelcase):
 
 def main():
     sentence=get_sentence()
-    words=split_sentence(sentence)
+    words=split_sentence(sentence)  
     camelCase=modify(words)
     display_sentence(camelCase)
 
--- ListOfClasses.py
+++ ListOfClasses.py
@@ -1,9 +1,12 @@
-classes = []
+#this program asks users number of classes they take 
+#then asks them to enter
+#then displays list of classes one per line
+classes = []    # empty lsit
 print('How many classes are you taking this semester?')
-numOfClasses=int(input())
+numOfClasses=int(input())   #stores number of classes
 for i in range(0,numOfClasses):
     subject=input('Enter the name of the class: ')
-    classes.append(subject)
+    classes.append(subject)     #adds each class to the classes list
 print('The classes you are taking are:')
 for sub in classes:
-    print(sub)
+    print(sub)  #prints each list
